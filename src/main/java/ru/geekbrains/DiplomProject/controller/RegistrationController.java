bd2f01a66c579df2869e7e9d9ba36453429355e9
Artem Privalov <rashen95@mail.ru>
2024-03-02 13:15:50 +0300

    v3

--- src/main/java/ru/geekbrains/DiplomProject/controller/RegistrationController.java
+++ src/main/java/ru/geekbrains/DiplomProject/controller/RegistrationController.java
@@ -1,9 +1,27 @@
 package ru.geekbrains.DiplomProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.geekbrains.DiplomProject.entity.User;
 
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
+    @GetMapping()
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping()
+    public String registerUser(@RequestParam("userName") String userName,
+                               @RequestParam("password") String password,
+                               Model model) {
+        System.out.printf("Пользователь %s с паролем %s зарегистрирован!%n", userName, password);
+        return "redirect:/login";
+    }
 }
---
+++ src/main/java/ru/geekbrains/DiplomProject/entity/User.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.DiplomProject.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
